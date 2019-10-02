867b6cd1dded3d37a43c526320384d6aee655cbb
华清松 <821034742@qq.com>
2019-10-02 16:53:31 +0800

    我的喜爱功能

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to Flutter',
+      theme: ThemeData(primaryColor: Colors.amber),
       home: WordWidgetFul(),
     );
   }
@@ -26,13 +27,50 @@ class WordWidgetFul extends StatefulWidget {
 
 class WordState extends State<WordWidgetFul> {
   final _suggestions = <WordPair>[];
+  final _saved = Set<WordPair>();
   final _biggerFont = const TextStyle(fontSize: 20.0);
+  final _smallerFont = const TextStyle(fontSize: 14.0, color: Colors.blue);
+
+  void _pushSave() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _saved.map(
+            (WordPair pair) {
+              return new ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final List<Widget> divided =
+              ListTile.divideTiles(context: context, tiles: tiles).toList();
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('我的喜欢'),
+            ),
+            body: ListView(
+              children: divided,
+            ),
+          );
+        },
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('我的APP'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.favorite),
+            onPressed: _pushSave,
+          )
+        ],
       ),
       body: _buildSuggestion(),
     );
@@ -47,18 +85,36 @@ class WordState extends State<WordWidgetFul> {
 
           final index = i ~/ 2;
           if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
+            _suggestions.addAll(generateWordPairs().take(4));
           }
           return _buildRow(_suggestions[index]);
         });
   }
 
   Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      subtitle: Text(
+        pair.asPascalCase,
+        style: _smallerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 }
